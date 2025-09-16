d16e3a64b631fed6ce49a846762f07203cfbc5d1
Beni-Bena <benib1489@gmail.com>
2025-09-16 20:27:46 +0100

    premiere modification

--- anniv/views.py
+++ anniv/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    liste = [
+     liste = [
         "Keren Siki","Sam Matia","Victoria Basemenane",
         "Christian Imani","Epaphras Makenene","Perside Lusakueno",
         "Melanie Masengu","Jr M'buyamba","Pascal Bunyori","Vainqueur Musangu",
         "Deodat Muhiya","Mounier Bena","Bellevie Katota","Mervedie Tsimba","Eliane Sibi",
         "Fideline Balibuno","Dr Luciane Sibi","Clareine Mulekera","Didier Ambunga","Cathia Abongi",
         "Lucie Kandolo","Jonathan Kanga","Bowa Mervedie","Cpa Redel","Jimmy Rais","Jed Milinganyo",
-        "Emmanuel Kasongo","Junior Malu","Manasseh","Cp Abel"
-        "Gloire Ashuza",
+        "Emmanuel Kasongo","Junior Malu","Manasseh","Cp Abel","Dr Freud","Adjany ","Percide Bombale",
+        "Gloire Ashuza","Andy Binaki","Grace Ndungi","Joed Matumba"
         ]
     liste *= 2
     return render(request, "index.html", {"names": liste})
